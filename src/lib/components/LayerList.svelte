29e49cd255434b580a57673308139c1870e7ff02
Kathrin Geilmann <16289455+kgeilmann@users.noreply.github.com>
2026-04-12 20:22:12 +0200

    Revert "feat: add trace mode to convert stroke paths to filled paths"
    This reverts commit 2a75cb98c184aff012bc6d77f7d0d29c9ea6c4b3.

--- src/lib/components/LayerList.svelte
+++ src/lib/components/LayerList.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { doodledialStore } from '$lib/stores/doodledial.svelte';
 	import RotationKnob from './RotationKnob.svelte';
-	import TraceButton from './TraceButton.svelte';
 
 	function handleToggle(layerId: string) {
 		doodledialStore.toggleVisibility(layerId);
@@ -102,10 +101,6 @@
 								{layer.name}
 							</span>
 							<div class="flex items-center gap-2 shrink-0">
-								<TraceButton
-									layerId={layer.id}
-									disabled={!layer.visible || doodledialStore.labelEditMode}
-								/>
 								<RotationKnob
 									value={layer.rotation}
 									onchange={(rotation) => handleRotationChange(layer.id, rotation)}
